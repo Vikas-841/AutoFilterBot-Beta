2b8bb8756a0943c2b3e8c1e83eacdde7d451b038
Vikas-841 <119723483+Vikas-841@users.noreply.github.com>
2023-11-07 10:43:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,108 +13,62 @@ def is_enabled(value, default):
         return default
 
 # Bot information
-API_ID = environ.get('API_ID', '25826048')
-if len(API_ID) == 0:
-    logging.error('API_ID is missing, exiting now')
-    exit()
-else:
-    API_ID = int(API_ID)
-API_HASH = environ.get('API_HASH', 'b486ee260537697fdfc56b2b61cbc049')
-if len(API_HASH) == 0:
-    logging.error('API_HASH is missing, exiting now')
-    exit()
-BOT_TOKEN = environ.get('BOT_TOKEN', '6882915473:AAFykN6NnKYxbV9sYDxo0orMjnEQBjg-L_A')
-if len(BOT_TOKEN) == 0:
-    logging.error('BOT_TOKEN is missing, exiting now')
-    exit()
-PORT = int(environ.get('PORT', '8080'))
+API_ID = 25826048
+API_HASH = "b486ee260537697fdfc56b2b61cbc049"
+BOT_TOKEN = "6882915473:AAFykN6NnKYxbV9sYDxo0orMjnEQBjg-L_A"
+PORT = 8080
 
 # Bot pics
-PICS = (environ.get('PICS', 'https://telegra.ph/file/58fef5cb458d5b29b0186.jpg https://telegra.ph/file/f0aa4f433132769f8970c.jpg https://telegra.ph/file/f515fbc2084592eca60a5.jpg https://telegra.ph/file/20dbdcffaa89bd3d09a74.jpg https://telegra.ph/file/6045ba953af4def846238.jpg')).split()
+PICS = [
+    "https://telegra.ph/file/58fef5cb458d5b29b0186.jpg",
+    "https://telegra.ph/file/f0aa4f433132769f8970c.jpg",
+    "https://telegra.ph/file/f515fbc2084592eca60a5.jpg",
+    "https://telegra.ph/file/20dbdcffaa89bd3d09a74.jpg",
+    "https://telegra.ph/file/6045ba953af4def846238.jpg"
+]
 
 # Bot Admins
-ADMINS = environ.get('ADMINS', '6324330202')
-if len(ADMINS) == 0:
-    logging.error('ADMINS is missing, exiting now')
-    exit()
-else:
-    ADMINS = [int(admins) for admins in ADMINS.split()]
+ADMINS = [6882915473]  # Replace with your admin IDs
 
 # Channels
-INDEX_CHANNELS = [int(index_channels) if index_channels.startswith("-") else index_channels for index_channels in environ.get('INDEX_CHANNELS', '').split()]
-AUTH_CHANNEL = [int(auth_channels) for auth_channels in environ.get('AUTH_CHANNEL', '-1002126379861').split()]
-LOG_CHANNEL = environ.get('LOG_CHANNEL', '-1002126379861')
-if len(LOG_CHANNEL) == 0:
-    logging.error('LOG_CHANNEL is missing, exiting now')
-    exit()
-else:
-    LOG_CHANNEL = int(LOG_CHANNEL)
-    
-SUPPORT_GROUP = environ.get('SUPPORT_GROUP', 'deathshot_support')
-if len(SUPPORT_GROUP) == 0:
-    logging.error('SUPPORT_GROUP is missing, exiting now')
-    exit()
-else:
-    SUPPORT_GROUP = int(SUPPORT_GROUP)
-    
-OPENAI_API = environ.get('OPENAI_API', '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')
-if len(OPENAI_API) == 0:
-    logging.error('OPENAI_API is missing, exiting now')
-    exit()
+INDEX_CHANNELS = [-1002126379861]
+AUTH_CHANNEL = [-1002126379861]
+LOG_CHANNEL = -1002126379861
+SUPPORT_GROUP = -1002126379861
 
 # MongoDB information
-DATABASE_URL = environ.get('DATABASE_URL', "mongodb+srv://bikashxd:bgtop@cluster0.gh1hqe3.mongodb.net/?retryWrites=true&w=majority")
-if len(DATABASE_URL) == 0:
-    logging.error('DATABASE_URL is missing, exiting now')
-    exit()
-DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Files')
+DATABASE_URL = "mongodb+srv://bikashxd:bgtop@cluster0.gh1hqe3.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_NAME = "Cluster0"
+COLLECTION_NAME = "Files"
 
 # Links
-SUPPORT_LINK = environ.get('SUPPORT_LINK', 'https://t.me/SL_Bots_Support')
-UPDATES_LINK = environ.get('UPDATES_LINK', 'https://t.me/SL_Bots_Updates')
+SUPPORT_LINK = "https://t.me/SL_Bots_Support"
+UPDATES_LINK = "https://t.me/SL_Bots_Updates"
 
 # Bot settings
-AUTO_FILTER = is_enabled((environ.get('AUTO_FILTER', "True")), True)
-IMDB = is_enabled((environ.get('IMDB', "True")), True)
-SPELL_CHECK = is_enabled(environ.get("SPELL_CHECK", "True"), True)
-SHORTLINK = is_enabled((environ.get('SHORTLINK', "False")), False)
-DELETE_TIME = int(environ.get('DELETE_TIME', 3600)) # Add time in seconds
-AUTO_DELETE = is_enabled((environ.get('AUTO_DELETE', "False")), False)
-WELCOME = is_enabled((environ.get('WELCOME', "False")), False)
-PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
-LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False"), False)
-LINK_MODE = is_enabled(environ.get("LINK_MODE", "True"), True)
-CACHE_TIME = int(environ.get('CACHE_TIME', 300))
+AUTO_FILTER = True
+IMDB = True
+SPELL_CHECK = True
+SHORTLINK = False
+DELETE_TIME = 3600  # Add time in seconds
+AUTO_DELETE = False
+WELCOME = False
+PROTECT_CONTENT = False
+LONG_IMDB_DESCRIPTION = False
+LINK_MODE = True
+CACHE_TIME = 300
 
 # Other
-IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", script.IMDB_TEMPLATE)
-FILE_CAPTION = environ.get("FILE_CAPTION", script.FILE_CAPTION)
-SHORTLINK_URL = environ.get("SHORTLINK_URL", "mdiskshortner.link")
-SHORTLINK_API = environ.get("SHORTLINK_API", "36f1ae74ba1aa01e5bd73bdd0bc22aa915443501")
-VERIFY_EXPIRE = int(environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
-IS_VERIFY = is_enabled(environ.get("IS_VERIFY", "False"), False)
-WELCOME_TEXT = environ.get("WELCOME_TEXT", script.WELCOME_TEXT)
-TUTORIAL = environ.get("TUTORIAL", "https://t.me/SL_Bots_Updates")
-INDEX_EXTENSIONS = [extensions.lower() for extensions in environ.get('INDEX_EXTENSIONS', 'mp4 mkv').split()]
+IMDB_TEMPLATE = script.IMDB_TEMPLATE
+FILE_CAPTION = script.FILE_CAPTION
+SHORTLINK_URL = "mdiskshortner.link"
+SHORTLINK_API = "36f1ae74ba1aa01e5bd73bdd0bc22aa915443501"
+VERIFY_EXPIRE = 86400  # Add time in seconds
+IS_VERIFY = False
+WELCOME_TEXT = script.WELCOME_TEXT
+TUTORIAL = "https://t.me/SL_Bots_Updates"
+INDEX_EXTENSIONS = ["mp4", "mkv"]
 
-# stream features vars
-BIN_CHANNEL = environ.get("BIN_CHANNEL", "")
-if len(BIN_CHANNEL) == 0:
-    logging.error('BIN_CHANNEL is missing, exiting now')
-    exit()
-else:
-    BIN_CHANNEL = int(BIN_CHANNEL)
-URL = environ.get("URL", "")
-if len(URL) == 0:
-    logging.error('URL is missing, exiting now')
-    exit()
-else:
-    if URL.startswith('https://'):
-        if not URL.endswith("/"):
-            URL += '/'
-    elif '.' in URL:
-        URL = f'http://{URL}:{PORT}/'
-    else:
-        logging.error('URL is not valid, exiting now')
-        exit()
+# Stream features vars
+BIN_CHANNEL = -1002126379861
+URL = "https://your-api-url-here/"
